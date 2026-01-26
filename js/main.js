0966cf22f20a5d4f746683c34ea1e614122adb03
Hazem Mahmoud <ehmage2002@gmail.co>
2026-01-26 20:29:23 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,86 +1,18 @@
+import { cardsData } from "./cardData.js";
+
 const cardGroup = document.getElementById("cardgroup");
 const searchInput = document.getElementById("search");
 const selectionInput = document.getElementById("select");
 
-const cardsData = [
-  {
-    id: 1,
-    img: "images/Chrono Classic Watch.png",
-    product: "Chrono Classic Watch",
-    price: "$250",
-    category: "Watches",
-    favorite: false,
-  },
-  {
-    id: 2,
-    img: "images/Noir Leather Watch.png",
-    product: "Noir Leather Watch",
-    price: "$180",
-    category: "Watches",
-    favorite: false,
-  },
-  {
-    id: 3,
-    img: "images/Silver Edge Watch.png",
-    product: "Silver Edge Watch",
-    price: "$300",
-    category: "Watches",
-    favorite: false,
-  },
-  {
-    id: 4,
-    img: "images/Minimal Steel Watch.png",
-    product: "Minimal Steel Watch",
-    price: "$220",
-    category: "Watches",
-    favorite: false,
-  },
-  {
-    id: 5,
-    img: "images/Oud Royale.png",
-    product: "Oud Royale",
-    price: "$220",
-    category: "Perfumes",
-    favorite: false,
-  },
-  {
-    id: 6,
-    img: "images/Bracelets.png",
-    product: "Silver Bracelets",
-    price: "$80",
-    category: "Jewelry",
-    favorite: false,
-  },
-  {
-    id: 7,
-    img: "images/Black Bracelets.png",
-    product: "Black Bracelets",
-    price: "$65",
-    category: "Jewelry",
-    favorite: false,
-  },
-  {
-    id: 8,
-    img: "images/Necklaces.png",
-    product: "Necklaces",
-    price: "$350",
-    category: "Jewelry",
-    favorite: false,
-  },
-  {
-    id: 9,
-    img: "images/Rings.png",
-    product: "Rings",
-    price: "$400",
-    category: "Jewelry",
-    favorite: false,
-  },
-];
-
 renderCards(cardsData);
 
-searchInput.addEventListener("input", (e) => {
-  const searchValue = e.target.value.toLowerCase();
+searchInput.addEventListener("input", (e) => searchHandler(e.target.value));
+selectionInput.addEventListener("input", (e) =>
+  searchHandler(searchInput.value),
+);
+
+function searchHandler(value) {
+  const searchValue = value.toLowerCase();
   const searchType = selectionInput.value;
 
   if (searchValue === "") {
@@ -97,7 +29,7 @@ searchInput.addEventListener("input", (e) => {
   });
 
   renderCards(filteredCards);
-});
+}
 
 function renderCards(data) {
   cardGroup.innerHTML = "";
@@ -148,3 +80,91 @@ function createCard({ id, img, product, price, category, favorite }) {
 
   return div;
 }
+
+// -----------------------------------------------------------
+
+const registermodal = document.getElementById("registermodal");
+const loginmodal = document.getElementById("loginmodal");
+const registerForm = document.getElementById("registerForm");
+const firstNameRegisterForm = document.getElementById("firstNameRegisterForm");
+const lastNameRegisterForm = document.getElementById("lastNameRegisterForm");
+const emailRegisterForm = document.getElementById("emailRegisterForm");
+const passwordRegisterForm = document.getElementById("passwordRegisterForm");
+
+let userInfo = {};
+const users = [];
+
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  users.push(userInfo);
+  console.log(users);
+  userInfo = {};
+  alert("Account Created Successfully!");
+  bootstrap.Modal.getInstance(registermodal).hide();
+  new bootstrap.Modal(loginmodal).show();
+});
+
+firstNameRegisterForm.addEventListener("input", () => {
+  userInfo.name =
+    firstNameRegisterForm.value + " " + lastNameRegisterForm.value;
+});
+
+lastNameRegisterForm.addEventListener("input", () => {
+  userInfo.name =
+    firstNameRegisterForm.value + " " + lastNameRegisterForm.value;
+});
+
+emailRegisterForm.addEventListener("input", () => {
+  userInfo.email = emailRegisterForm.value;
+
+  const isValid = /^\w+@[a-zA-Z]+\.[a-zA-Z]{3}$/.test(emailRegisterForm.value);
+
+  if (isValid) {
+    emailRegisterForm.classList.add("is-valid");
+    emailRegisterForm.classList.remove("is-invalid");
+  } else {
+    emailRegisterForm.classList.add("is-invalid");
+    emailRegisterForm.classList.remove("is-valid");
+  }
+});
+
+passwordRegisterForm.addEventListener("input", () => {
+  userInfo.password = passwordRegisterForm.value;
+});
+
+// -----------------------------------------------------------
+
+const loginForm = document.getElementById("loginForm");
+const emailLoginForm = document.getElementById("emailLoginForm");
+const passwordLoginForm = document.getElementById("passwordLoginForm");
+let currentUser = {};
+const userLogin = {};
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userIndex = users.findIndex((c) => c.email === userLogin.email);
+  if (userIndex != -1) {
+    if (userLogin.password === users[userIndex].password) {
+      console.log("successfully");
+      currentUser = {...userLogin}
+      bootstrap.Modal.getInstance(loginmodal).hide();
+    } else {
+      passwordLoginForm.classList.add("is-invalid");
+    }
+  } else {
+    emailLoginForm.classList.add("is-invalid");
+  }
+});
+
+emailLoginForm.addEventListener("input", () => {
+  userLogin.email = emailLoginForm.value;
+  emailLoginForm.classList.remove("is-invalid");
+});
+
+passwordLoginForm.addEventListener("input", () => {
+  userLogin.password = passwordLoginForm.value;
+  passwordLoginForm.classList.remove("is-invalid");
+});
+
+// -----------------------------------------------------------
+
